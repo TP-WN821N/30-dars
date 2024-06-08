68b202531b2eed7c4c7d131786a281540eb6ed4b
TP-WN821N <azikmelor7705@gmail.com>
2024-06-08 19:40:30 +0500

    header

--- app.js
+++ app.js
@@ -1,6 +1,3 @@
-const user = querySelector('.user')
-
-
 const myTime = () => {
   let now = new Date()
 
@@ -12,6 +9,34 @@ const myTime = () => {
   document.querySelector('.magic_min').innerHTML = minute;
   document.querySelector('.magic_second').innerHTML = seconds;
 }
-let interval = setInterval(myTime, 1000)
-myTime()
+let interval = setInterval(myTime, 1000);
+const menu = document.querySelector('.navMenu')
+const headerRight = document.querySelector('.header_right')
+const xmark = document.getElementById('xmark')
+const backdrop = document.getElementById('backdrop')
+console.log(backdrop)
+
+menu.addEventListener('click', () => {
+  menu.classList.toggle('dn')
+  xmark.classList.toggle('dn')
+  headerRight.classList.add('toggle_sidebar')
+  backdrop.classList.add('overflow')
+
+})
+xmark.addEventListener('click', () => {
+  menu.classList.toggle('dn')
+  xmark.classList.toggle('dn')
+  headerRight.classList.remove('toggle_sidebar')
+  backdrop.classList.remove('overflow')
+})
+
+if (window.matchMedia("(max-width: 1023px)").matches) {
+  menu.classList.remove('dn')
+  headerRight.classList.add('dn')
+} else {
+  menu.classList.add('dn')
+  headerRight.classList.remove('dn')
+}
+
+
 
