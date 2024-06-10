37a234a3d74cd5757836337fffd4f73dcfd87c4b
TP-WN821N <azikmelor7705@gmail.com>
2024-06-11 00:03:26 +0500

    artist page

--- app.js
+++ app.js
@@ -1,20 +1,20 @@
 const myTime = () => {
   let now = new Date()
 
-  let hour = now.getHours() < 10 ? '0' + now.getHours() : now.getHours();
+  let soat = now.getHours() < 10 ? '0' + now.getHours() : now.getHours();
   let minute = now.getMinutes() < 10 ? '0' + now.getMinutes() : now.getMinutes();
   let seconds = now.getSeconds() < 10 ? '0' + now.getSeconds() : now.getSeconds();
 
-  document.querySelector('.magic_hours').innerHTML = hour;
+  document.querySelector('.magic_hours').innerHTML = soat;
   document.querySelector('.magic_min').innerHTML = minute;
   document.querySelector('.magic_second').innerHTML = seconds;
+  console.log(`${soat}:${minute}:${seconds}`)
 }
 let interval = setInterval(myTime, 1000);
 const menu = document.querySelector('.navMenu')
 const headerRight = document.querySelector('.header_right')
 const xmark = document.getElementById('xmark')
 const backdrop = document.getElementById('backdrop')
-console.log(backdrop)
 
 menu.addEventListener('click', () => {
   menu.classList.toggle('dn')
@@ -30,13 +30,18 @@ xmark.addEventListener('click', () => {
   backdrop.classList.remove('overflow')
 })
 
-if (window.matchMedia("(max-width: 1023px)").matches) {
+if (window.matchMedia("(max-width: 910px)").matches) {
   menu.classList.remove('dn')
   headerRight.classList.add('dn')
 } else {
   menu.classList.add('dn')
   headerRight.classList.remove('dn')
 }
-
-
-
+const header = document.querySelector('.header');
+window.addEventListener('scroll', () => {
+  if (window.scrollY > 10) {
+    header.classList.add('header_shodow')
+  } else {
+    header.classList.remove('header_shodow')
+  }
+})
